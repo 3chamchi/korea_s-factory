745ec77ba91190a884dc9a1b95fbf206c21ae984
chamchi <44095761+3chamchi@users.noreply.github.com>
2022-09-28 20:47:28 +0900

    3주차 실습 코드, 퀴즈

--- week3/section_01.py
+++ week3/section_01.py
@@ -3,6 +3,10 @@
 # 파이썬 데이터 타입(자료형) 실습
 ######################################################
 
+# a = 3
+# True
+# true
+
 '''
 [파이썬 자료형]
    int : 정수
@@ -46,24 +50,80 @@
 '''
 
 # int : 정수
+# int a = 40
+a = 20
+b = 40
+sum = a + b
+print(b)
+print(a + b)
+b = 'awda'  # or ""
 
-
+print(9999 * 3)
+age = 20
+print(age - 60)
+print(type(age))
 # float : 실수
+print('float===============')
+a = 20.0
+print(a)
+type(a)
+print(type(a))
 
+print(a * 3)
+print(type(a * 3))
 
 # str : 문자열
-
+# print("str============")
+print('str============')
+a = 'abc'
+print(a)
+type(a)
+print(type(a))
+# 인덱싱
+print(a[0])
+print(a[1])
+print(a[2])
+# 슬라이싱 abc
+print(a[1:3])
+phone = '010-0000-1234'
+print(phone[9:13])
+print('20' + '5')
+print(20 + 5)
 
 # list : 리스트
-
+print('list============')
+#    0  1  2  3  ...
+a = [1, 2, 3, 4, 'a', '22', 40.0]
+print(a)
+print(a[3])
+print(a[3:5])
 
 # tuple : 튜플
-
-
+print('tuple============')
+a = (1, 2, 3, 4)
+print(a)
+print(a[3])
+print(a[3:6])
 # set : 집합
 
-
 # dict : 사전
-
+print('dict=====')
+a = {'키': '값'}
+a = {
+    '이름': '홍길동',
+    '나이': 20,
+    '번호': '000-0000-0000'
+}
+print(a)
+print(a['이름'])
 
 # 형변환
+a = [1,2,3,4]
+print(type(a))
+print(tuple(a))
+a1 = '20'
+a2 = int(a1)
+print(a1)
+print(a2)
+print(type(a1))
+print(type(a2))
