005cc31dac508f9d49816a6746c5a771e6b7c1bf
chamchi <44095761+3chamchi@users.noreply.github.com>
2022-10-11 21:36:44 +0900

    4주차 실습 코드

--- week4/section_01.py
+++ week4/section_01.py
@@ -3,59 +3,136 @@
 # 제어문 if 실습
 ######################################################
 
-print(type(True))
-print(type(False))
-
 # 기본 형식
 # if 기본 형태 - 참(또는 거짓)인 경우
+if True:
+    print('True')
+
+if False:
+    print('False')
 
 # if 기본 형태2 - else
+# if True:
+if False:
+    print('True')
+else:
+    print('False')
 
 # 관계 연산자
 # >, >=, <, <=, ==, !=
-
-
 # int 변수 a, b
+a = 30
+b = 50
+if a < b:
+    print('A < B')
+a = 30
+b = 15
+if 30 < 15:
+    print('A < B')
 
 # == 양 변이 같을 때 참.
+if 20 == 20:
+    print('20 == 20')
 
 # != 양 변이 다를 때 참.
+if 20 != 20:
+    print('20 != 20')
+    print('if는 들여쓰기 기준으로 실행1')
+    print('if는 들여쓰기 기준으로 실행2')
+    print('if는 들여쓰기 기준으로 실행3')
+print('end if.')
 
 # > 왼쪽이 클때 참.
+if 20 > 20:
+    print('20 > 20')
 
 # >= 왼쪽이 크거나 같을 때 참.
+if 20 >= 20:
+    print('20 >= 20')
 
 # < 오른쪽이 클 때 참.
+if 20 < 20:
+    print('20 < 20')
 
 # <= 오른쪽이 크거나 같을 때 참.
+if 20 <= 20:
+    print('20 <= 20')
 
 # 참 거짓 예시
-# 참: "값", [값], (값), {값}, 1
+# 참: "값" or '값', [값], (값), {값}, 1
 # 거짓: "", [], (), {}, 0, None
 
-# 조건이 참인 경우 - movie_type = ''
+# 조건이 거짓인 경우 - movie_type = ''
+movie_type = ''
+if movie_type:
+    print(movie_type, '입력하셨습니다.')
+else:
+    print('장르를 입력해주세요.')
 
+# 조건이 참인 경우 - movie_type = '코미디'
+movie_type = '코미디'
+if movie_type:
+    print(movie_type, '입력하셨습니다.')
+else:
+    print('장르를 입력해주세요.')
 
-# 조건이 거짓인 경우 - movie_type = 'Comedy'
+print('A', 'B', 'C', end='\n\n\n')
+print('A', 'B', 'C')
 
 # 논리 연산자 and, or, not
+score = 88
+print(80 < score and score < 90)
+
+score = 88
+print(80 < 88 or 90 < 88)
+#        참        거짓
+# if 80 < 88 or 90 < 88: ...
+
+print(not True)
 
 # 산술, 관계, 논리 우선순위 - 산술 > 관계 > 논리 순서 적용
-
-
-# 복수의 조건이 모두 참일 경우
+print((80 < 88) or (90 < 88))
 
 # 점수와 학점
+score = 91
+if 90 < score:
+    print('A')
+elif 80 < score:  # 80 < score and score < 90
+    print('B')
+else:
+    print('F')
 
 # 관리자 사이트 권한
-
-
-# 다중 조건문
-# 자격증 점수
-
+is_active = True
+is_admin = True
+if is_active and is_admin:
+    print('로그인 처리')
+else:
+    print('로그인 실패')
 
 # 중첩 조건문
 # 신체검사 키, 몸무게
-
+age = 20
+kg = 30
+if 19 < age:
+    if 40 < kg:
+        print('지원 가능')
+    else:
+        print('몸무게 미달, 지원 불가.')
+else:
+    print('나이 미달, 지원 불가.')
 
 # in, not in
+movie_type = ['코미디', '액션', '드라마']
+print('코미디' in movie_type)
+print('스릴러' not in movie_type)
+
+
+# print 사용 방법 - 추천
+# name = 'korea'
+# display_string = name + '님 안녕하세요.'
+# display_string = f'{name}님 안녕하세요.222'
+# print(f'{name}님 안녕하세요.') # 1
+# print(name, '님 안녕하세요.') # 2
+# print(name + '님 안녕하세요.') # 3
+# print(display_string)
--- week4/section_02.py
+++ week4/section_02.py
@@ -5,9 +5,55 @@
 
 # 기본 반복문 사용(while, for)
 # while
+while False:  # True
+    print('while True')
+
+var_input = ''
+# while var_input != 'exit':
+#     var_input = input('입력 값: ')
+#     print(f'{var_input} 입력!')
+# 입력에 따른 값 처리
 
 # for
+movie_type = ['코미디', '액션', '드라마']
+
+for movie in movie_type:
+    print(movie)
+
+movie_type = ['코미디', '액션', '드라마']
+for movie in movie_type:
+    if '코미디' == movie:
+        print(True)
 
 # 1 ~ 100 합 계산
+numbers = [1, 2, 3, 4, 5, 6, ]
+number_sum = 0
+for index in numbers:
+    number_sum = number_sum + index
+print(number_sum)
 
-# 리스트 중 int 3이 포함되었는지 확인
+for index in range(100):
+    print(index)
+print(range(100))
+
+for index in range(10):
+    for index2 in range(10):
+        print(f'{index} x {index2} = {index * index2}')
+
+number_sum = 0
+for number in range(100):
+    print(number)
+    number_sum = number_sum + number
+print(number_sum)
+
+print(sum(range(100)))
+
+# for -> while
+number = 0
+while number < 100:
+    print(number)
+    number = number + 1
+
+number_sum = 0
+for number in range(100):
+    print(number)
