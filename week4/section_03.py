ae24c42c26c9d56416992ffab561f14e8c4bc71f
chamchi <44095761+3chamchi@users.noreply.github.com>
2022-10-12 03:20:31 +0900

    3주차 퀴즈 풀이 코드

--- week4/section_03.py
+++ week4/section_03.py
@@ -15,7 +15,19 @@
 # C : 70 이상 80 미만
 # D : 60 이상 70 미만
 # F : 60 미만
+# score = 90
+score = int(input('점수를 입력해주세요 : '))
 
+if score >= 90:
+    print('A')
+elif score >= 80:
+    print('B')
+elif score >= 70:
+    print('C')
+elif score >= 60:
+    print('D')
+else:
+    print('F')
 
 # 2. 업다운 게임을 구현하시오.
 # 기본
@@ -26,6 +38,23 @@
 # 심화
 # 정답 숫자를 입력 받아서 진행, "0"을 입력 받은 경우 종료
 # 참고 기능 : input(), exit(), random()
+import random
+
+random_number = random.randrange(50)
+print(f'random_number : {random_number}')
+
+print('[업다운 게임] 0~50까지 숫자 중 입력해주세요.')
+
+while True:
+    input_number = int(input('숫자를 입력해주세요 : '))
+
+    if random_number == input_number:
+        print('정답')
+        break
+    elif random_number > input_number:
+        print('업')
+    else:
+        print('다운')
 
 # 3. 자판기 소프트웨어를 구현하시오.
 # 자판기 상품 콜라 1500원 사이다 1500원 물 800원
@@ -33,3 +62,27 @@
 # 1. 현금 입력(input() 활용)
 # 2. 상품 선택
 # 3. 상품, 거스름돈 출력(print() 활용)
+products = {
+    1: {'NAME': '콜라', 'PRICE': 1500, },
+    2: {'NAME': '사이다', 'PRICE': 1500, },
+    3: {'NAME': '물', 'PRICE': 800, },
+}
+
+print('==================')
+print('상품 목록')
+print('1. 콜라 1500원')
+print('2. 사이다 1500원')
+print('3. 물 800원')
+print('==================')
+# for key in products:
+#     print(f'{key}. {products[key]["NAME"]} {products[key]["PRICE"]}원')
+
+money = int(input('자판이 현금을 투입해주세요 : '))
+choice = int(input('구매할 상품의 번호를 입력해주세요 : '))
+
+if money < products[choice]['PRICE']:
+    print('현금이 부족합니다.')
+else:
+    product_name = products[choice]["NAME"]
+    change = money - products[choice]["PRICE"]
+    print(f'{product_name} 상품 출력. 잔돈 {change}')
