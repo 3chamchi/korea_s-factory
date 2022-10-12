388377dd47b62f083532d09939462ac4bcdd03be
chamchi <44095761+3chamchi@users.noreply.github.com>
2022-10-12 17:40:16 +0900

    5주차 실습 주석

---
+++ week5/section_01.py
@@ -0,0 +1,35 @@
+######################################################
+# Week5
+# Section 01
+# 함수 function 실습
+######################################################
+
+# Parameter == 매개변수 == 인자
+# Argument == 인수
+
+### 함수 선언
+# def function_name(Parameter):
+#   code...
+#   return data
+
+### 함수 선언 설명
+# def 함수명(파라미터):
+#   실행 코드
+#   return 응답 데이터
+
+### *args, **kwargs 이해
+### def function(*args, **kwargs): ...
+
+### Typing - 타입힌팅
+### https://docs.python.org/3/library/typing.html
+
+
+### 함수 주석
+###
+
+### 내장함수, 외장함수
+
+### 내장함수
+### https://docs.python.org/ko/3/library/functions.html
+
+###
