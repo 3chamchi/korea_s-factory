a71c2890377bcca0d5513c88ba3ae03930fd890e
chamchi <3chamchi@gmail.com>
2022-11-02 20:55:58 +0900

    8주차 실습 코드

---
+++ week8/posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class Post(models.Model):
+    # 제목
+    title = models.CharField(max_length=250)
+    # 내용
+    content = models.TextField()
+    # 작성자
+    created_by = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    tag = models.CharField(max_length=30)
+
+
